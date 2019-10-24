cef7e8b38c7422f0faba758899c6813aed141159
Evgeniy <e.sukhalintsev@yandex.ru>
2019-10-24 21:38:24 +0300

    init site

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+"use strict";
+
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var less = require("gulp-less");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var server = require("browser-sync").create();
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var del = require("del");
+
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+    imagemin.optipng({
+        optimizationLevel: 3
+      }),
+    imagemin.jpegtran({
+        progressive: true
+      }),
+    imagemin.svgo()
+  ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.scss", gulp.series("css"));
+  gulp.watch("source/*.html").on("change", server.reload);
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({
+      quality: 90
+    }))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("start", gulp.series("css", "server"));
---
+++ source/js/menu.js
@@ -0,0 +1,12 @@
+var navMain = document.querySelector('.page-header');
+var navToggle = document.querySelector('.page-header__toggle');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('page-header--closed')) {
+    navMain.classList.remove('page-header--closed');
+    navMain.classList.add('page-header--opened');
+  } else {
+    navMain.classList.add('page-header--closed');
+    navMain.classList.remove('page-header--opened');
+  }
+});
